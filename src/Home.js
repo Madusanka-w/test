822d5ffc7d30725a2603e2336cad78e9faad31f6
Madusanka-w <mwattale@gmail.com>
2023-06-14 08:23:56 +0100

    use effect

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [name, setName] = useState('Navod')
-    const [age, setAge] = useState(25)
+    const [name, setName] = useState('Mario');
 
-    const handleClick = (e) => {
-        setName('Madusanka')
-        setAge(30)
+    const [blogs, setBlogs] = useState([
+        {title: 'My New Website', body: 'lsflsfjsla sfssf', author: 'Mario', id: 1},
+        {title: 'My New Project', body: 'my new blog sflafslfj', author: 'Madusanka', id: 2},
+        {title: 'New web page', body: 'new web page .....', author: 'Mario', id: 3}
+    ])
+    
+    const handleDelete = (id) => {
+        const newBlog = blogs.filter(blog => blog.id !== id)
+        setBlogs(newBlog)
     }
+
+    useEffect(() => {
+        console.log('UseEffect ran');
+        console.log(name);
+    }, [name])
 
 
     return ( 
         <div className="hom">
-            <h2>Home Page</h2>
-            <p>{ name } is { age } old.</p>
-            <button onClick={handleClick}>Click Me</button>
+           <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+           <button onClick={() => setName('Hesh')}>Change Name</button>
+           <p>{ name }</p>
         </div>
      );
 }
