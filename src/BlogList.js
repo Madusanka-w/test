394a00034e78219e29852c8bdbe69f0e4db0a91e
Madusanka-w <mwattale@gmail.com>
2023-06-14 12:45:00 +0100

    created custom hook

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,18 +1,17 @@
-const BlogList = ({ blogs, title, handleDelete}) => {
-
-    return ( 
-        <div className="blog-list">
-            <h2>{ title }</h2>
-            {blogs.map((blog) => (
-            <div className="blog-preview" key={blog.id}>
-                <h2>{ blog.title }</h2>
-                <p><b>Written By :</b> {blog.author }</p>
-                <button onClick={() => (handleDelete(blog.id))}>Delete Blog</button>
-            </div>
-        ))}
-
+const BlogList = ({ blogs, title}) => {
+  return (
+    <div className="blog-list">
+      <h2>{title}</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>
+            <b>Written By :</b> {blog.author}
+          </p>
         </div>
-     );
-}
- 
+      ))}
+    </div>
+  );
+};
+
 export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,18 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
+  
+  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
 
-    const [name, setName] = useState('Mario');
+  return (
+    <div className="home">
+      { error && <div>{ error }</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+    </div>
+  );
+};
 
-    const [blogs, setBlogs] = useState([
-        {title: 'My New Website', body: 'lsflsfjsla sfssf', author: 'Mario', id: 1},
-        {title: 'My New Project', body: 'my new blog sflafslfj', author: 'Madusanka', id: 2},
-        {title: 'New web page', body: 'new web page .....', author: 'Mario', id: 3}
-    ])
-    
-    const handleDelete = (id) => {
-        const newBlog = blogs.filter(blog => blog.id !== id)
-        setBlogs(newBlog)
-    }
-
-    useEffect(() => {
-        console.log('UseEffect ran');
-        console.log(name);
-    }, [name])
-
-
-    return ( 
-        <div className="hom">
-           <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
-           <button onClick={() => setName('Hesh')}>Change Name</button>
-           <p>{ name }</p>
-        </div>
-     );
-}
- 
 export default Home;
